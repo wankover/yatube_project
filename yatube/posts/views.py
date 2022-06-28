4a264a478be331e68f185357c40783cbd0295f96
Иван Черненко <wankover@yandex.ru>
2022-06-28 07:52:49 +0300

    Добавление админки

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,23 @@
+
+
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Главная страница проекта Yatube')
+    template = 'posts/index.html'
+    title = 'Последние обновления на сайте'
+    text = 'Это главная страница проекта Yatube'
+    context = {'title' : title,
+               'text' : text,
+               }
+    return render(request, template, context)
 
 def group_posts(request, slug):
-    return HttpResponse(f'Список постов {slug}')
+    text = 'Здесь будет информация о группах проекта Yatube'
+    title = 'Лев Толстой – зеркало русской революции.'
+    context = {'title' : title,
+               'text' : text,
+               }
+    template = 'posts/group_list.html'
+    
+    return render(request, template, context)
