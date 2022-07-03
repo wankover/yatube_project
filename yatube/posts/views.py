bd2bbb5dabe575c8a43bc133cbfde016d51db584
Иван Черненко <wankover@yandex.ru>
2022-07-03 19:47:26 +0300

    Создание модели Group

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,23 +1,17 @@
-
-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 # Create your views here.
 def index(request):
-    template = 'posts/index.html'
-    title = 'Последние обновления на сайте'
-    text = 'Это главная страница проекта Yatube'
-    context = {'title' : title,
-               'text' : text,
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {'posts' : posts,
                }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 def group_posts(request, slug):
-    text = 'Здесь будет информация о группах проекта Yatube'
-    title = 'Лев Толстой – зеркало русской революции.'
-    context = {'title' : title,
-               'text' : text,
-               }
-    template = 'posts/group_list.html'
-    
-    return render(request, template, context)
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]    
+    context = {'group' : group,
+               'posts' : posts,
+               }    
+    return render(request, 'posts/group_list.html', context)
